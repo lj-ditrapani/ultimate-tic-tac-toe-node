250147ce718ff47568ddbd43d84ff3fb4f3965a6
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2019-05-18 12:02:10 -0400

    Add prettier & tslint

--- src/models.ts
+++ src/models.ts
@@ -1,10 +1,6 @@
 type ActiveBoard = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 'A'
 
-class Player {
-  constructor(
-    public readonly ws: WebSocket
-  ) {}
-}
+type Player = WebSocket
 
 type Cell = 'empty' | 'player1' | 'player2'
 
@@ -35,14 +31,12 @@ export const init = new Init()
 export class ReadyPlayer1 {
   public readonly status: 'readyPlayer1' = 'readyPlayer1'
 
-  constructor(
-    public readonly player1: Player
-  ) {}
+  constructor(public readonly player1: Player) {}
 }
 
 export class Turn {
   public readonly status: 'turn' = 'turn'
-  
+
   constructor(
     public readonly gameState: GameState,
     public readonly activePlayer: 1 | 2,
@@ -52,20 +46,17 @@ export class Turn {
 
 export class GameOver {
   public readonly status: 'gameOver' = 'gameOver'
-  
+
   constructor(
     public readonly gameState: GameState,
-    public readonly winner: 1 | 2 | 'T',
+    public readonly winner: 1 | 2 | 'T'
   ) {}
 }
 
 export class Reset {
   public readonly status: 'reset' = 'reset'
 
-  constructor(
-    public readonly gameState: GameState,
-    public readonly resetPlayr: 1 | 2
-  ){}
+  constructor(public readonly gameState: GameState, public readonly resetPlayr: 1 | 2) {}
 }
 
 export type Status = Init | ReadyPlayer1 | Turn | GameOver | Reset
