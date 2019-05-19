9336836c5e6064b44a23ea521693d71c3f97793f
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2019-05-19 14:36:14 -0400

    On connection; tell client what they are
    (p1, p2 or spectator)

--- src/game.ts
+++ src/game.ts
@@ -36,12 +36,21 @@ type InterpretConnection = (ws: WebSocket, status: Status) => Event
 export const interpretConnection = (ws: WebSocket, status: Status): Event => {
   switch (status.statusType) {
     case 'init':
+      sendEntity(ws, 'player1')
       return new NewStatus(new ReadyPlayer1(ws), ws)
     case 'readyPlayer1':
+      sendEntity(ws, 'player2')
       return new NewStatus(initialTurn(status.player1, ws), ws)
     default:
+      sendEntity(ws, 'spectator')
       return new SpectatorJoined(ws)
   }
+}
+
+type Entity = 'player1' | 'player2' | 'spectator'
+
+const sendEntity = (ws: WebSocket, entity: Entity): void => {
+  ws.send(`Entity: ${entity}`)
 }
 
 export type MessageHandler = (ws: WebSocket, message: string) => void
--- todo.md
+++ todo.md
@@ -1,7 +1,2 @@
 State.update
     -> broadcast to players (conditional on status)
-
-onConnection
-    - update state/spectators if needed
-    - send entity (p1, p2, or spectator) back to client ws
-    - broadcast new state to all ws
