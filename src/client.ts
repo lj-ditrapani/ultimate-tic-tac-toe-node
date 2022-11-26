7ccbb9d70381fbe52337d89dcf362200c10b6ef0
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2022-11-26 16:01:40 -0500

    Remove node-fetch
    Node 18 has a built-in fetch implementation so we don't need to pollyfill

--- src/client.ts
+++ src/client.ts
@@ -1,9 +1,5 @@
 import { createTRPCProxyClient, httpBatchLink } from '@trpc/client'
 import type { AppRouter } from './server'
-import fetch from 'node-fetch'
-
-const globalAny = global as unknown as { fetch: typeof fetch }
-globalAny.fetch = fetch
 
 const trpc = createTRPCProxyClient<AppRouter>({
   links: [
