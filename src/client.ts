c9c1d8bab6abcf2b718c47db3fe6aed9d06b35af
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2022-11-26 15:47:48 -0500

    Get rid of 'any' lint warning

--- src/client.ts
+++ src/client.ts
@@ -2,7 +2,7 @@ import { createTRPCProxyClient, httpBatchLink } from '@trpc/client'
 import type { AppRouter } from './server'
 import fetch from 'node-fetch'
 
-const globalAny = global as any
+const globalAny = global as unknown as { fetch: typeof fetch }
 globalAny.fetch = fetch
 
 const trpc = createTRPCProxyClient<AppRouter>({
