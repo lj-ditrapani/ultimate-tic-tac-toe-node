48b6b9e3155ca68cb637f9911d66c140ee7fb95d
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2022-11-27 17:26:06 -0500

    Get server host name from env var; instead of hard coded

--- src/client.ts
+++ src/client.ts
@@ -3,14 +3,24 @@ import { Game } from './client/game.js'
 import { makeUi } from './client/ui.js'
 import type { AppRouter } from './server'
 import fetch from 'node-fetch'
+import * as dotenv from 'dotenv'
 
+dotenv.config()
 const g = globalThis as unknown as { fetch: typeof fetch }
 g.fetch = fetch
+const host = process.env['host']
+
+if (!host) {
+  throw new Error(
+    'host environment variable must be defined ' +
+      'and point to ultimate tic-tac-toe server',
+  )
+}
 
 export const trpcClient = createTRPCProxyClient<AppRouter>({
   links: [
     httpBatchLink({
-      url: 'http://localhost:3000',
+      url: `http://${host}:3000`,
     }),
   ],
 })
--- todo.md
+++
@@ -1,1 +0,0 @@
-- get host from env var; not hard coded
