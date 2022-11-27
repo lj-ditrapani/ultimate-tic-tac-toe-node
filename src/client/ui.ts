1281c9439e27aa3f64950d93c71cf54635cb1340
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2022-11-27 16:58:54 -0500

    Draw complete board background

--- src/client/ui.ts
+++ src/client/ui.ts
@@ -1,7 +1,7 @@
 import { colors, ITermGrid, makeTermGrid } from 'term-grid-ui'
 
 const height = 24
-const width = 20
+const width = 19
 
 export const makeUi = (): Ui => {
   const tg = makeTermGrid(height, width)
@@ -28,6 +28,11 @@ export class Ui {
     this.tg.set(0, 0, 'X', this.xC, this.boarder)
     this.tg.set(10, 7, 'O', this.oC, this.boardBg)
     this.tg.set(10, 8, 'O', this.oC, this.active)
+    for (const x of [...Array(width).keys()]) {
+      for (const y of [...Array(width).keys()]) {
+        this.tg.set(y, x, ' ', this.textC, this.boardBg)
+      }
+    }
     this.drawHBoarder(0)
     this.draw2HLinesFor3HBoards(2)
     this.drawHBoarder(6)
@@ -35,6 +40,13 @@ export class Ui {
     this.drawHBoarder(12)
     this.draw2HLinesFor3HBoards(14)
     this.drawHBoarder(18)
+    this.drawVBoarder(0)
+    this.draw2VLinesFor3VBoards(2)
+    this.drawVBoarder(6)
+    this.draw2VLinesFor3VBoards(8)
+    this.drawVBoarder(12)
+    this.draw2VLinesFor3VBoards(14)
+    this.drawVBoarder(18)
     this.tg.draw()
   }
 
@@ -59,9 +71,20 @@ export class Ui {
     }
   }
 
+  private drawVBoarder(x: number) {
+    for (const y of [...Array(width).keys()]) {
+      this.tg.set(y, x, ' ', this.textC, this.boarder)
+    }
+  }
+
   private draw2HLinesFor3HBoards(y: number) {
     this.draw3HLinesIn1Row(y)
     this.draw3HLinesIn1Row(y + 2)
+  }
+
+  private draw2VLinesFor3VBoards(x: number) {
+    this.draw3VLinesIn1Column(x)
+    this.draw3VLinesIn1Column(x + 2)
   }
 
   private draw3HLinesIn1Row(y: number) {
@@ -70,9 +93,24 @@ export class Ui {
     this.drawHLine(y, 13)
   }
 
+  private draw3VLinesIn1Column(x: number) {
+    this.drawVLine(1, x)
+    this.drawVLine(7, x)
+    this.drawVLine(13, x)
+  }
+
   private drawHLine(y: number, x: number) {
     for (const i of [x, x + 2, x + 4]) {
       this.tg.set(y, i, '-', this.textC, this.boardBg)
+    }
+    for (const i of [x + 1, x + 3]) {
+      this.tg.set(y, i, '+', this.textC, this.boardBg)
+    }
+  }
+
+  private drawVLine(y: number, x: number) {
+    for (const i of [y, y + 2, y + 4]) {
+      this.tg.set(i, x, '|', this.textC, this.boardBg)
     }
   }
 }
